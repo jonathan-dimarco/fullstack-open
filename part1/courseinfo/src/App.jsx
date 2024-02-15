58ce874d6811dbc9936f7887032548ddcf4c14f8
Jonathan_DiMarco <jonathandimarcoq@gmail.com>
2024-02-15 21:30:53 +0100

    ex 1.1 solved

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,3 +1,24 @@
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <p>{props.part} {props.excercises}</p>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of excercises {props.a + props.b + props.c}</p>
+  )
+}
+
+
+
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -9,17 +30,11 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course}/>
+      <Content part={part1} excercises={exercises1}/>
+      <Content part={part2} excercises={exercises2}/>
+      <Content part={part3} excercises={exercises3}/>
+      <Total a={exercises1} b={exercises2} c={exercises1}/>
     </div>
   )
 }
